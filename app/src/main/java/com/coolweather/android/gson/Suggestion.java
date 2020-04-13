ea59f6d676c7d7884e8f557a9cfca38e6c646aa8
Taue9 <1296458483@qq.com>
2020-04-13 21:27:07 +0800

    a

--- app/src/main/java/com/coolweather/android/gson/Suggestion.java
+++ app/src/main/java/com/coolweather/android/gson/Suggestion.java
@@ -3,27 +3,20 @@ package com.coolweather.android.gson;
 import com.google.gson.annotations.SerializedName;
 
 public class Suggestion {
-
     @SerializedName("comf")
-    public Comfort comfort;
-
+    public Comfort comfort; // 舒适度
     @SerializedName("cw")
-    public CarWash carWash;
-
-    @SerializedName("txt")
-    public Sport sport;
-
-    public class Comfort {
+    public CarWash carWash; // 洗车建议
+    public Sport sport; // 运动建议
+    public class Comfort{
         @SerializedName("txt")
         public String info;
     }
-
-    public class CarWash {
+    public class CarWash{
         @SerializedName("txt")
         public String info;
     }
-
-    public class Sport {
+    public class Sport{
         @SerializedName("txt")
         public String info;
     }
